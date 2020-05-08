de1446d70e6a40fbb83eba840f7dd213a6d1e4e2
Sharath Sahadevan <sharath_sahadevan@hotmail.com>
2020-05-07 21:15:43 -0500

    Updated client

--- bin/resttest.sh
+++ bin/resttest.sh
@@ -1,4 +1,4 @@
-set +x
+-set +x
 curl --data "dev&dev-pass" http://localhost:5701/hazelcast/rest/management/cluster/state
 echo "Nodes: \n"
 curl --data "dev&dev-pass" http://localhost:5701/hazelcast/rest/management/cluster/nodes
@@ -14,3 +14,4 @@ curl --data "dev&dev-pass" http://localhost:5702//hazelcast/health/cluster-state
 
 curl --data "dev&dev-pass" http://localhost:5701//hazelcast/health/cluster-size
 curl --data "dev&dev-pass" http://localhost:5702//hazelcast/health/cluster-size
+
--- src/main/java/client/GettingStartedClient.java
+++ src/main/java/client/GettingStartedClient.java
@@ -5,10 +5,29 @@ import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IMap;
  
+/* 
+ * Usage:
+ *    GettingStartedClient ip port
+ *    or 
+ *    GettingStartedClient 
+ *       to connect to defaults which are 
+ */
 public class GettingStartedClient {
     public static void main(String[] args) {
         ClientConfig clientConfig = new ClientConfig();
+        
+        int numberOfArgs=args.length;
+        if (numberOfArgs == 2)
+        {
+        	String ip = args[0];
+        	String port = args[1];
+        	clientConfig.addAddress( args[0] + ":" + args[1]);
+        }
+        else
+        {
+        // Connect to localhost 
         clientConfig.addAddress("127.0.0.1:5701");
+        }
         HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);
         IMap map = client.getMap("customers");
         System.out.println("Map Size:" + map.size());
